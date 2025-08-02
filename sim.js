4d189cc8ba45d85643d0368105b8badb5b798e04
GreenieGuest <papootreasure@yahoo.com>
2025-08-02 13:20:33 -0500

    Add files via upload

---
+++ sim.js
@@ -0,0 +1,8 @@
+//stuff and thangs -rick grimes
+//pls forgive me im kinda bad at this
+const proceed = document.getElementById("proceed");
+const simtext = document.getElementById("status");
+
+function check() {
+	simtext.innerHTML = "prankd";
+}
